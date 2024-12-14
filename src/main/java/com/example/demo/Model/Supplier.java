cbae19fbd503ea77037f4a5471b70343790d6451
u1081795708 <126284518+u1081795708@users.noreply.github.com>
2024-12-14 18:04:51 +0300

    new d

---
+++ src/main/java/com/example/demo/Model/Supplier.java
@@ -0,0 +1,21 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.Access;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+
+public class Supplier {
+    @Id
+    private int id;
+    private String name;
+}
